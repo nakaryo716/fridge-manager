b115824ee904a0744475736ae592f05ba710670c
ryonakaya <nakayaryo0716@gmail.com>
2024-03-02 20:50:51 +0900

    データベーススキーマの再設計

--- api/migrations/20240301060338_item.sql
+++ api/migrations/20240301060338_item.sql
@@ -1,7 +1,12 @@
-CREATE TABLE IF NOT EXISTS fridge-db
-(
-    id          SERIAL PRIMARY KEY,
-    name        TEXT    NOT NULL,
-    deadline
-    used        BOOLEAN NOT NULL DEFAULT false
-)
+CREATE TYPE EXDATE AS (
+    year    INT,
+    month   INT,
+    day     INT
+);
+
+CREATE TABLE IF NOT EXISTS item (
+    id              SERIAL PRIMARY KEY,
+    name            TEXT    NOT NULL,
+    expiration_date EXDATE NOT NULL,
+    used            BOOLEAN NOT NULL DEFAULT false
+);
