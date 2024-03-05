476f1c56044ac550a10bd50d5186ebb034bd874c
ryo <nakayaryo0716@gmail.com>
2024-03-05 14:24:53 +0900

    データベーススキーマの再設計

--- api/migrations/20240301060338_item.sql
+++ api/migrations/20240301060338_item.sql
@@ -1,12 +1,8 @@
-CREATE TYPE EXDATE AS (
-    year    INT,
-    month   INT,
-    day     INT
-);
-
 CREATE TABLE IF NOT EXISTS item (
-    id              SERIAL PRIMARY KEY,
-    name            TEXT    NOT NULL,
-    expiration_date EXDATE NOT NULL,
-    used            BOOLEAN NOT NULL DEFAULT false
+    id          SERIAL  PRIMARY KEY,
+    name        TEXT    NOT NULL,
+    year        INTEGER NOT NULL,
+    month       INTEGER NOT NULL,
+    day         INTEGER NOT NULL,
+    used        BOOLEAN NOT NULL DEFAULT FALSE
 );
