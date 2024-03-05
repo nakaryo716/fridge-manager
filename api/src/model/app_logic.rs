409f4f6e665d399cdf003a6466b03ebf31e7e085
ryo <nakayaryo0716@gmail.com>
2024-03-05 16:08:03 +0900

    アプリケーションSQLロジックの実装

--- api/src/model/app_logic.rs
+++ api/src/model/app_logic.rs
@@ -6,7 +6,7 @@ use async_trait::async_trait;
 pub trait RepositoryForDb: Clone + std::marker::Send + std::marker::Sync + 'static {
     async fn create(&self, payload: CreateItem) -> Result<Item, RepositoryError>;
     async fn read(&self, id: i32) -> Result<Item, RepositoryError>;
-    async fn read_all(&self) -> Result<Item, RepositoryError>;
+    async fn read_all(&self) -> Result<Vec<Item>, RepositoryError>;
     async fn update(&self, id: i32, payload: UpdateItem) -> Result<Item, RepositoryError>;
     async fn delete(&self, id: i32) -> Result<(), RepositoryError>;
 }
@@ -15,17 +15,54 @@ pub trait RepositoryForDb: Clone + std::marker::Send + std::marker::Sync + 'stat
 impl RepositoryForDb for ItemRepository {
     // postの内容をdatabaseにinsert
     async fn create(&self, payload: CreateItem) -> Result<Item, RepositoryError> {
-        Ok(todo!())
+        let item = sqlx::query_as::<_, Item>(r#"
+INSERT INTO item (name, year, month, day)
+VALUES ($1, $2, $3, $4)
+RETURNING *
+        "#)
+        .bind(payload.name)
+        .bind(payload.expiration_date.year)
+        .bind(payload.expiration_date.month)
+        .bind(payload.expiration_date.day)
+        .fetch_one(&self.pg_pool)
+        .await
+       .map_err(|e| match e {
+            sqlx::Error::RowNotFound => RepositoryError::NotFoud(0),
+            _ => RepositoryError::Unexpected,     
+       })?;
+        Ok(item)
     }
 
     // 任意のidのデータをdatabaseから取得
     async fn read(&self, id: i32) -> Result<Item, RepositoryError> {
-        Ok(todo!())
+        let item = sqlx::query_as::<_, Item>(r#"
+SELECT * FROM item
+WHERE id = $1
+        "#)
+        .bind(id)
+        .fetch_one(&self.pg_pool)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => RepositoryError::NotFoud(id),
+            _ => RepositoryError::Unexpected,
+        })?;
+
+        Ok(item)
     }
 
     // データベースにあるすべてのデータの取得
-    async fn read_all(&self) -> Result<Item, RepositoryError> {
-        Ok(todo!())
+    async fn read_all(&self) -> Result<Vec<Item>, RepositoryError> {
+        let item = sqlx::query_as::<_, Item>(r#"
+SELECT * FROM item
+        "#)
+        .fetch_all(&self.pg_pool)
+        .await
+        .map_err(|e| match  e  {
+            sqlx::Error::RowNotFound => RepositoryError::NotFoud(0),
+            _ => RepositoryError::Unexpected,
+        })?;
+
+        Ok(item)
     }
 
     // 任意のidのデータを更新
