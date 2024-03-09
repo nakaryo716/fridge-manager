f83236d95bb7bb41e2f4f5101e71406f656f9722
ryonakaya <nakayaryo0716@gmail.com>
2024-03-09 13:01:04 +0900

    add validation error

--- api/src/error_type.rs
+++ api/src/error_type.rs
@@ -1,10 +1,38 @@
+use axum::{
+    extract::rejection::JsonRejection,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use thiserror::Error;
 
-// エラーの定義
+// リポジトリエラーの定義
 #[derive(Debug, Error)]
 pub enum RepositoryError {
     #[error("NotFound id is: {0}")]
     NotFoud(i32),
     #[error("Unexpected Error")]
     Unexpected,
+}
+
+// Validationエラーの定義
+#[derive(Debug, Error)]
+pub enum ServerError {
+    #[error(transparent)]
+    ValidationError(#[from] validator::ValidationErrors),
+
+    #[error(transparent)]
+    AxumJsonRejection(#[from] JsonRejection),
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        match self {
+            ServerError::ValidationError(_) => {
+                let message = format!("Input validation error: [{self}]").replace('\n', ", ");
+                (StatusCode::BAD_REQUEST, message)
+            }
+            ServerError::AxumJsonRejection(_) => (StatusCode::BAD_REQUEST, self.to_string()),
+        }
+        .into_response()
+    }
 }
