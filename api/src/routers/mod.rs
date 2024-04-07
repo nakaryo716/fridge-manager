215caf77b723b83fd413f323106812604022d1fd
ryonakaya <nakayaryo0716@gmail.com>
2024-04-07 14:55:52 +0900

    implement CORS layer

--- api/src/routers/mod.rs
+++ api/src/routers/mod.rs
@@ -3,9 +3,9 @@ use crate::{
     store::RepositoryForDb,
 };
 use axum::{
-    routing::{get, post},
-    Router,
+    http::{header::CONTENT_TYPE, HeaderValue, Method}, routing::{get, post}, Router
 };
+use tower_http::cors::{Any, CorsLayer};
 use std::sync::Arc;
 
 // ルーティングの設定
@@ -18,4 +18,11 @@ pub fn app<T: RepositoryForDb>(repository: T) -> Router {
             get(get_item).put(update_item).delete(delete_item),
         )
         .with_state(Arc::new(repository))
+        .layer(
+            CorsLayer::new()
+                // "http://localhost:5173".parse::<HeaderValue>().unwrap()
+                .allow_origin(Any)
+                .allow_methods(vec![Method::POST, Method::GET, Method::PUT, Method::DELETE])
+                .allow_headers(vec![CONTENT_TYPE])
+        )
 }
