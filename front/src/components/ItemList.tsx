bf4b5498cf0e4bd9cb5e043b00e5db5e9432fbd7
ryonakaya <nakayaryo0716@gmail.com>
2024-04-14 19:22:05 +0900

    impl modal window

--- front/src/components/ItemList.tsx
+++ front/src/components/ItemList.tsx
@@ -1,7 +1,6 @@
-import { Box, Button, Checkbox, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { TrackedFood, UpdateFoodPayload } from "../types/itemType";
-import { useState } from "react";
-import { EditItem } from "./EditItem";
+import { FoodItem } from "./FoodItem";
 
 
 type Props = {
@@ -13,28 +12,6 @@ type Props = {
 export const ItemList = (props: Props) => {
     const {foods, onUpdateHandle, onDeleteHandle} = props;
 
-    const onClickCheckBox = async (Updates: TrackedFood) => {
-        const {id, name, expiration_date, used} = Updates;
-
-        const payload: UpdateFoodPayload = {
-            name,
-            expiration_date,
-            used: !used
-        };  
-        await onUpdateHandle(id, payload);
-    };
-
-    const onClickDelete = async (id: number) => {
-        await onDeleteHandle(id);
-    };
-
-    const [modalFlag, setModalFlag] = useState(false);
-    const modalOpen = (id: number) => {
-        console.log(id);
-        setModalFlag(true);
-    }
-    const modalClose = () => setModalFlag(false);
-    
     return (
         <div>
             <TableContainer component={Paper} sx={{width: 1200, margin: "auto"}}>
@@ -50,21 +27,7 @@ export const ItemList = (props: Props) => {
                     <TableBody>
                         {foods.map((food) => {
                             return(
-                                <TableRow key={food.id}>
-                                    <TableCell align="left"><Checkbox onChange={() => onClickCheckBox(food)} checked={food.used}/>{food.name}</TableCell>
-                                    <TableCell align="left">{food.expiration_date}</TableCell>
-                                    <TableCell align="left">
-                                        <Button key={food.id} variant="contained" sx={{backgroundColor: "mediumturquoise", "&:hover":{backgroundColor: "darkcyan"}}} onClick={() => modalOpen(food.id)}>編集</Button>
-                                        <Modal open={modalFlag} onClose={modalClose}>
-                                            <Box sx={style}>
-                                                <EditItem food={food} onUpdateHandle={onUpdateHandle} setModalFlag={setModalFlag}></EditItem>
-                                            </Box>
-                                        </Modal>
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        <Button variant="contained" sx={{backgroundColor: "pink", "&:hover":{backgroundColor: "hotpink"}}} onClick={() => onClickDelete(food.id)}>削除</Button>
-                                    </TableCell>
-                                </TableRow>
+                                <FoodItem food={food} onUpdateHandle={onUpdateHandle} onDeleteHandle={onDeleteHandle} />
                             )
                         })}
                     </TableBody>
@@ -72,17 +35,4 @@ export const ItemList = (props: Props) => {
             </TableContainer>
         </div>
     );
-};
-
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 1200,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
 };
