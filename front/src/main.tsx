2d35438499b39bc6e44009122977a5582d4c45dc
ryonakaya <nakayaryo0716@gmail.com>
2024-05-09 17:36:44 +0900

    ライトモードとダークモードを時間によって切り替えるようにした

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,15 +5,28 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { App } from './App';
 import "./index.css";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+// 18時以降は自動でダークモードになるように設定する関数
+const isDark = () => {
+  const currentHour = new Date().getHours();
+
+  if (currentHour > 18) {
+    return createTheme({
+      palette: {
+        mode: 'dark',
+      },
+    })
+  } else {
+    return createTheme({
+      palette: {
+        mode: 'light',
+      },
+    })
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={isDark()}>
       <CssBaseline />
         <App />
     </ThemeProvider>
