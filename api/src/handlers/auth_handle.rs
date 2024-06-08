87225b3eac4add3fd6f94f785c91296bea71fe3f
ryonakaya <nakayaryo0716@gmail.com>
2024-06-08 17:50:39 +0900

    認証関連のハンドラー追加

--- api/src/handlers/auth_handle.rs
+++ api/src/handlers/auth_handle.rs
@@ -1,1 +1,34 @@
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
+use crate::middleware::auth::{Auth, AuthError, CreateUser, Credential, UsersRepository};
+
+pub async fn sign_up(
+    State(user_repository): State<UsersRepository>,
+    Json(payload): Json<CreateUser>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let res = user_repository
+        .create_user(payload)
+        .await
+        .map_err(|e| match e {
+            AuthError::AlredyExists => StatusCode::BAD_REQUEST,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        })?;
+
+    Ok((StatusCode::OK, Json(res)))
+}
+
+pub async fn sign_in(
+    State(user_repository): State<UsersRepository>,
+    Json(credential): Json<Credential>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let res = user_repository
+        .verify_user(credential)
+        .await
+        .map_err(|e| match e {
+            AuthError::UserNotFound => StatusCode::NOT_FOUND,
+            AuthError::Defferentpassword => StatusCode::BAD_REQUEST,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        })?;
+
+    Ok((StatusCode::OK, Json(res)))
+}
