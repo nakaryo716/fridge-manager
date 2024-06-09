0dbad2ef9834484d13d87f1c1a6145381e360b65
ryonakaya <nakayaryo0716@gmail.com>
2024-06-09 14:42:46 +0900

    セッションで保存するのはユーザidとユーザーnameだけに修正

--- api/src/middleware/session.rs
+++ api/src/middleware/session.rs
@@ -3,7 +3,7 @@ use crate::AppState;
 use super::auth::User;
 use axum::{async_trait, extract::FromRef};
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use sqlx::{prelude::FromRow, PgPool};
 use thiserror::Error;
 use uuid::Uuid;
 
@@ -13,6 +13,11 @@ pub enum SessionError {
     NotFound,
     #[error("unexpected error")]
     Unexpected,
+}
+#[derive(Debug, Clone, Serialize, FromRow)]
+pub struct SessionInfo {
+    pub user_id: i32,
+    pub user_name: String,
 }
 
 #[derive(Debug, Clone)]
@@ -36,29 +41,28 @@ where
     type SessionId;
     type Error;
 
-    async fn create_session(&self, target_user: T) -> Result<Self::SessionId, Self::Error>;
-    async fn verify_session(&self, session_id: S) -> Result<Self::UserInfo, Self::Error>;
+    async fn create_session(&self, target_user: &T) -> Result<Self::SessionId, Self::Error>;
+    async fn verify_session(&self, session_id: &S) -> Result<Self::UserInfo, Self::Error>;
 }
 
 #[async_trait]
 impl SessionManage<'_, User, String> for SessionPool {
-    type UserInfo = User;
+    type UserInfo = SessionInfo;
     type SessionId = String;
     type Error = SessionError;
 
-    async fn create_session(&self, target_user: User) -> Result<Self::SessionId, Self::Error> {
+    async fn create_session(&self, target_user: &User) -> Result<Self::SessionId, Self::Error> {
         let session_id = Uuid::new_v4().to_string();
 
         sqlx::query(
             r#"
 INSERT INTO session 
-(session_id, user_info) VALUES ($1, $2)
-RETURNING *
+(session_id, user_id, user_name) VALUES ($1, $2, $3)
         "#,
         )
         .bind(&session_id)
         .bind(target_user.user_id)
-        .bind(target_user.user_name)
+        .bind(target_user.user_name.clone())
         .execute(&self.pool)
         .await
         .map_err(|_e| SessionError::Unexpected)?;
@@ -66,10 +70,10 @@ RETURNING *
         Ok(session_id)
     }
 
-    async fn verify_session(&self, session_id: String) -> Result<Self::UserInfo, Self::Error> {
-        let user_info = sqlx::query_as::<_, User>(
+    async fn verify_session(&self, session_id: &String) -> Result<Self::UserInfo, Self::Error> {
+        let session_info = sqlx::query_as::<_, SessionInfo>(
             r#"
-SELECT * FROM session 
+SELECT user_id, user_name FROM session 
 WHERE session_id = $1
         "#,
         )
@@ -78,7 +82,7 @@ WHERE session_id = $1
         .await
         .map_err(|_e| SessionError::Unexpected)?;
 
-        user_info.ok_or(SessionError::NotFound)
+        session_info.ok_or(SessionError::NotFound)
     }
 }
 
