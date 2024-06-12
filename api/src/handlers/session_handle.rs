8049ea6662e5b4eb089065f818870e83f79dda30
ryonakaya <nakayaryo0716@gmail.com>
2024-06-12 22:25:19 +0900

    SessionData extractorのテスト用ハンドラーの追加

--- api/src/handlers/session_handle.rs
+++ api/src/handlers/session_handle.rs
@@ -1,27 +1,10 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
-use axum_extra::extract::CookieJar;
+use axum::{http::StatusCode, response::IntoResponse, Json};
 
-use crate::middleware::session::{SessionError, SessionManage, SessionPool};
+use crate::middleware::session_extract::SessionData;
 
-use super::auth_handle::SESSION_ID;
-
+// SessionDataエキストラクターのテストのためのハンドラー
 pub async fn is_session(
-    jar: CookieJar,
-    State(session_store): State<SessionPool>,
+    SessionData(user_info): SessionData,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let value = jar.get(SESSION_ID).map(|cookie| cookie.value().to_owned());
-
-    match value {
-        Some(cookie_value) => {
-            let user = session_store
-                .verify_session(&cookie_value)
-                .await
-                .map_err(|e| match e {
-                    SessionError::NotFound => StatusCode::NOT_FOUND,
-                    SessionError::Unexpected => StatusCode::INTERNAL_SERVER_ERROR,
-                })?;
-            Ok((StatusCode::OK, Json(user)))
-        }
-        None => Err(StatusCode::NON_AUTHORITATIVE_INFORMATION),
-    }
+    Ok((StatusCode::OK, Json(user_info)))
 }
