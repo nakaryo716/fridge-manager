489458a975f7eb9d52c2a858046d34fc81cdf04e
ryonakaya <nakayaryo0716@gmail.com>
2024-06-13 20:16:55 +0900

    トレイト定義のファイル変更(src/model.rs->src/model/repository.rs)

--- api/src/model.rs
+++ api/src/model.rs
@@ -1,28 +1,2 @@
-use std::{error::Error, fmt::Debug};
-
-use axum::async_trait;
-use serde::{Deserialize, Serialize};
-use sqlx::{FromRow, Row};
-use validator::Validate;
-
 pub mod repository;
 
-// DBに対して一般的なCRUD操作を実装させるトレイト
-// 戻り値は任意の種類(Pg, MySql, SqLite)から得たデータが
-// Json化することができることをトレイト境界として指定している
-#[async_trait]
-pub trait CrudForDb<'a, S, T, U>: Clone + std::marker::Send + std::marker::Sync + 'static
-where
-    S: Row,
-    T: Deserialize<'a> + Validate + Clone,
-    U: Deserialize<'a> + Validate + Clone,
-{
-    type Response: Serialize + Deserialize<'a> + FromRow<'a, S>;
-    type Error: Debug + Error;
-
-    async fn create(&self, payload: T) -> Result<Self::Response, Self::Error>;
-    async fn read(&self, id: i32) -> Result<Self::Response, Self::Error>;
-    async fn read_all(&self) -> Result<Vec<Self::Response>, Self::Error>;
-    async fn update(&self, id: i32, payload: U) -> Result<Self::Response, Self::Error>;
-    async fn delete(&self, id: i32) -> Result<(), Self::Error>;
-}
