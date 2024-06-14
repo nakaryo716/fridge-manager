96fdd2b6ad1a124d785e904ae2124a875267423a
ryonakaya <nakayaryo0716@gmail.com>
2024-06-14 22:46:32 +0900

    セッションの確認をするapi呼び出し実装

---
+++ front/src/api/session.ts
@@ -0,0 +1,11 @@
+import { SessionInfo } from "../types/middleware";
+
+export const isSession = async (): Promise<SessionInfo> => {
+    const res = await fetch("http://localhost:3000/fridge", {
+        method: "GET",
+        credentials: "include",
+    });
+
+    const responseJson: SessionInfo = await res.json();
+    return responseJson;
+}
