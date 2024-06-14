c09ed186f1022bb0e39ebbd75d17601dbd65845a
ryonakaya <nakayaryo0716@gmail.com>
2024-06-14 22:45:53 +0900

    cookieを送れるようにcredentialを追加

--- front/src/api/callApi.ts
+++ front/src/api/callApi.ts
@@ -6,6 +6,7 @@ export const postFoodApi = async (payload: NewFoodPayload) => {
         headers: {
             "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify(payload),
     });
 
@@ -18,7 +19,10 @@ export const postFoodApi = async (payload: NewFoodPayload) => {
 };
 
 export const getAllFoodsApi = async () => {
-    const res = await fetch("http://localhost:3000/fridge");
+    const res = await fetch("http://localhost:3000/fridge", {
+        method: "GET",
+        credentials: "include",
+    });
 
     if (!res) {
         throw new Error("Could not get items");
@@ -34,6 +38,7 @@ export const updateFoodApi = async (id: number, payload: UpdateFoodPayload) => {
         headers: {
             "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify(payload),
     });
 
@@ -48,6 +53,7 @@ export const updateFoodApi = async (id: number, payload: UpdateFoodPayload) => {
 export const deleteFoodApi = async (id: number) => {
     const res = await fetch(`http://localhost:3000/fridge/${id}`, {
         method: "DELETE",
+        credentials: "include",
     });
 
     if (!res) {
