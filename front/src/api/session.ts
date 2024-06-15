2a51b3b2ceeee4e5c110df6ba9833b06b5bc6ecd
ryonakaya <nakayaryo0716@gmail.com>
2024-06-15 23:35:31 +0900

    叩くAPIへのurlの修正

--- front/src/api/session.ts
+++ front/src/api/session.ts
@@ -1,7 +1,7 @@
 import { SessionInfo } from "../types/middleware";
 
 export const isSession = async (): Promise<SessionInfo> => {
-    const res = await fetch("http://localhost:3000/fridge", {
+    const res = await fetch("http://localhost:3000/is_session", {
         method: "GET",
         credentials: "include",
     });
