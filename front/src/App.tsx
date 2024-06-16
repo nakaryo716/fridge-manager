e73cb983ae469b2c925987141ee93a77f3ad9581
ryonakaya <nakayaryo0716@gmail.com>
2024-06-16 23:07:14 +0900

    非同期関数修正

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { isSession } from "./api/session";
 import { FoodApp } from "./components/FoodApp"
 import { SignIn } from "./components/SignIn"
+import { SignUp } from "./components/SignUp";
 
 export const App = () => {
   const [haveSession, setHaveSession] = useState(false);
@@ -9,6 +10,11 @@ export const App = () => {
   useEffect(() => {
     (
       async () => {
+        // セッションがあるかどうか確認
+        // 個の非同期関数の戻り値は列挙型にした方がいいのか？
+        // Some(session) / None
+        // セッションがあればアプリケーションページにリダイレクト
+        // なければログインページにリダイレクト
         const res = await isSession();
         if (res) {
           setHaveSession(true);
