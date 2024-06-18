efc0a68f985305adf11924b80953536f2e410541
ryonakaya <nakayaryo0716@gmail.com>
2024-06-18 23:03:06 +0900

    ルーティング処理の追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "react";
-import { isSession } from "./api/session";
 import { FoodApp } from "./components/FoodApp"
-import { SignIn } from "./components/SignIn"
-import { SignUp } from "./components/SignUp";
 
 export const App = () => {
-  const [haveSession, setHaveSession] = useState(false);
-
-  useEffect(() => {
-    (
-      async () => {
-        // セッションがあるかどうか確認
-        // 個の非同期関数の戻り値は列挙型にした方がいいのか？
-        // Some(session) / None
-        // セッションがあればアプリケーションページにリダイレクト
-        // なければログインページにリダイレクト
-        const res = await isSession();
-        if (res) {
-          setHaveSession(true);
-        }
-      }
-    )();
-  }, []);
-
   return(
-      haveSession ? <FoodApp /> : <SignIn />
+    <FoodApp />
   )
 }
