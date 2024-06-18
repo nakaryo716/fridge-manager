9f3e0b83354fe793a67c23f47daef581acd9fa12
ryonakaya <nakayaryo0716@gmail.com>
2024-06-18 23:04:29 +0900

    ルーティング処理追加

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,6 +5,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { App } from './App';
 import "./index.css";
 import { CookiesProvider } from 'react-cookie';
+import { RouterProvider, createBrowserRouter, redirect } from 'react-router-dom';
+import { SignIn } from './components/SignIn';
+import { SignUp } from './components/SignUp';
+import { SessionValue, isSession } from './api/session';
 
 // 18時以降は自動でダークモードになるように設定する関数
 const isDark = () => {
@@ -25,12 +29,34 @@ const isDark = () => {
   }
 }
 
+const router = createBrowserRouter([
+  {
+    path: "/app",
+    element: <App />,
+    loader: async () => {
+      const res = await isSession();
+      if (res == SessionValue.None) {
+        return redirect("/sign_in");
+      }
+      return null
+    }
+  },
+  {
+    path: "/sign_in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign_up",
+    element: <SignUp />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={isDark()}>
       <CssBaseline />
         <CookiesProvider>
-          <App />
+          <RouterProvider router={router} />
         </CookiesProvider>
     </ThemeProvider>
   </React.StrictMode>
