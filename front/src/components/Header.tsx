cd14c8f8a03d1bcf245f324d80fbf3853d2c39a0
ryonakaya <nakayaryo0716@gmail.com>
2024-06-23 17:32:49 +0900

    ヘッダーの色変更+サインアウトのhover色追加

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -9,12 +9,12 @@ export const Header = (props: Props) => {
 
     return(
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{backgroundColor: "#003366"}}>
+        <AppBar position="static" sx={{backgroundColor: "#131921"}}>
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 賞味・消費期限マネージャー
             </Typography>
-            <Button color="inherit" onClick={signOutHandle}>サインアウト</Button>
+            <Button color="inherit" onClick={signOutHandle} sx={{ "&:hover":{ backgroundColor: "#3f526d"} }}>サインアウト</Button>
           </Toolbar>
         </AppBar>
       </Box>
