85ea1bdcf3a7d476cf9c83f51154558f123089ea
ryonakaya <nakayaryo0716@gmail.com>
2024-06-26 19:52:39 +0900

    サインイン/アップのapiレスポンスをvoidに変更

--- front/src/api/auth.ts
+++ front/src/api/auth.ts
@@ -1,30 +1,38 @@
-import { Credentials, NewUser, User } from "../types/middleware";
+import { Credentials, NewUser } from "../types/middleware";
 
-export const signUP = async (newUser: NewUser): Promise<User> => {
-    const res = await fetch("http://localhost:3000/sign_up",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newUser),
-    });
-
-    const responseJson: User = await res.json();
-    return responseJson;
+export const signUP = async (newUser: NewUser): Promise<void> => {
+    try {
+        const res = await fetch("http://localhost:3000/sign_up",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser),
+        });
+        if (!res.ok) {
+            throw new Error("sign up error");
+        }
+    } catch {
+        throw new Error("singup error");
+    }
 }
 
-export const signIn = async (credential: Credentials): Promise<User> => {
-    const res = await fetch("http://localhost:3000/sign_in",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(credential),
-    });
-
-    const responseJson: User = await res.json();
-    return responseJson;
+export const signIn = async (credential: Credentials): Promise<void> => {
+    try {
+        const res = await fetch("http://localhost:3000/sign_in",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+                body: JSON.stringify(credential),
+            });
+        if (!res.ok) {
+            throw new Error("sign error");
+        }
+    } catch {
+        throw new Error("unexpected error");
+    }
 }
 
 export const signOut = async () => {  
